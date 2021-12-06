68c52e020205d6c03b87f157230f7e8c8e625d60
Jose <jose.mercado@davinci.edu.ar>
2021-12-06 14:56:14 -0300

    Llenando la clase Tetraedro regular

--- src/TetraedroRegular.java
+++ src/TetraedroRegular.java
@@ -1,5 +1,27 @@
 public class TetraedroRegular extends FiguraTridimensional {
 
+    private double arista;
 
+    private static final int caras = 4;
 
+    public TetraedroRegular(double arista) {
+        this.arista = arista;
+    }
+
+    public TetraedroRegular() {
+        this.arista = 1;
+    }
+
+    public abstract double CalcularVolumen {
+        double volumen = (Math.pow(2, (1/2)) * ((Math.pow(arista, 3))/12);
+    }
+
+    public abstract double CalcularSuperficie {
+        double volumen = (Math.pow(3, (1/2))) * (Math.pow(arista, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "La Figura es un Tetraedro Regular";
+    }
 }
