4601198763bb04dc9c0252a839c25335ce40d80f
Jose <jose.mercado@davinci.edu.ar>
2021-12-06 15:03:20 -0300

    Llenando la clase Cubo

--- src/Cubo.java
+++ src/Cubo.java
@@ -1,5 +1,26 @@
 public class Cubo extends FiguraTridimensional {
 
+    private double lado;
+    private static final int caras = 6;
 
+    public Cubo(double lado) {
+        this.lado = lado;
+    }
 
+    public Cubo() {
+        this.lado = 1;
+    }
+
+    public abstract double CalcularVolumen {
+        double volumen = Math.pow(lado, 3);
+    }
+
+    public abstract double CalcularSuperficie {
+        double superficie = 6 * (Math.pow(lado, 2));
+    }
+
+    @Override
+    public String toString() {
+        return "La Figura es un Cubo";
+    }
 }
